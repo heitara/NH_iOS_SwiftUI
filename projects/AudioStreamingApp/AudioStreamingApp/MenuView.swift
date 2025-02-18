797132b8109e394595a20ad3e5aa709fc5168483
Emil Atanasov <heitara@gmail.com>
2025-02-18 19:44:04 +0200

    feat: use custom layout view

--- projects/AudioStreamingApp/AudioStreamingApp/MenuView.swift
+++ projects/AudioStreamingApp/AudioStreamingApp/MenuView.swift
@@ -7,20 +7,31 @@
 
 import SwiftUI
 
+struct CustomStack: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        CGSize(width: 300, height: 100)
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        var x = 0.0
+        for subview in subviews {
+            subview.place(at: bounds.origin.applying(CGAffineTransform(translationX: x, y: 0)), proposal: proposal)
+            x += 120.0
+        }
+    }
+    
+    
+}
+
 struct MenuView: View {
     var movies: [Movie] = readJSON()
     let gradient = LinearGradient(colors: [.pink, .cyan], startPoint: .top, endPoint: .bottomLeading)
     var body: some View {
-        VStack {
+        CustomStack {
             Circle()
                 .fillAndHeight(gradient: gradient)
-            GeometryReader { geometry in
-                Text("Size: \(geometry.size)")
-                Button("Hello") {
-                    
-                }
-                .offset(CGSize(width: geometry.size.width / 2, height: geometry.size.height / 2))
-            }
+            Circle()
+                .fillAndHeight(gradient: gradient)
             Circle()
                 .fillAndHeight(gradient: gradient)
         }
