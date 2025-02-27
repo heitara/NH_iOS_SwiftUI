fc75aebc2eb2146418d9a403f50534f1a1cd20f7
Emil Atanasov <heitara@gmail.com>
2025-02-27 18:19:27 +0200

    feat: create a basic SwiftUIView that uses a UIKit component (UIView)

---
+++ projects/AudioStreamingApp/AudioStreamingApp/WebView/WebView.swift
@@ -0,0 +1,35 @@
+//
+//  WebView.swift
+//  AudioStreamingApp
+//
+//  Created by Emil Atanasov on 02/27/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+        
+        let webView = WKWebView(frame: .zero, configuration: config)
+        
+        // TODO: add navigation delegate ...
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+#Preview {
+    WebView(url: URL(string: "https://google.com")!)
+}
